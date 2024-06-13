05f01164e83f0552a106e587bdf9fe38d2109b81
Kamily <costakamily3@gmail.com>
2024-06-13 15:51:13 -0300

    Criação a estrutura inicial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+//import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+//import sobreScreen from './views/Sobre'
+//import ProdutoScreen from './views/Produtos'
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+     
+       
+    </NavigationContainer>
+  );
+}
+
+export default App;[]
