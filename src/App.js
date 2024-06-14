d50717fd375df95d3f6c41d0892435f2e1d94c0a
Kamily <costakamily3@gmail.com>
2024-06-14 07:50:58 -0300

    Auterações iniciis de teste

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-//import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-//import sobreScreen from './views/Sobre'
-//import ProdutoScreen from './views/Produtos'
+import sobreScreen from './views/Sobre'
+import ProdutoScreen from './views/Produtos'
+import PedidosScreen from './views/Pedidos'
+import CarinhoScreen from './views/Carinho'
 
 const Tab = createBottomTabNavigator();
 
